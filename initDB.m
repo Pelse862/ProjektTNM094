e1e91297517b0ecf0b72f9448a8c090484a8f993
pelse862 <pelse862@student.liu.se>
2016-02-23 12:21:37 +0100

    fixed first tile verion

--- initDB.m
+++ initDB.m
@@ -2,12 +2,19 @@
 %Set image dimensions
 w = 32; h = 32;
 
-images = zeros(w*h, 16);
-for i = 1:50
-    
-    image = imread(sprintf('DB/%d.jpg',i));
+
+for n = 1:50
+
+    image = imread(sprintf('DB/%d.jpg',n));
 
     subImageTemp = imresize((im2double(image)), [w, h]);
     
-    
+    temp = rgb2lab(subImageTemp);
+    L = mean2(temp(:,:,1));
+    a = mean2(temp(:,:,2));
+    b = mean2(temp(:,:,3));
+    LABvalue{n} = [L,a,b, n];    
+
 end
+
+save('DB.mat', 'LABvalue');
--- main.m
+++ main.m
@@ -1,8 +1,8 @@
 image = imread('bild.jpg');
 image = double(image);
+%load db
+load('DB.mat');
 
-height = 30;
-width = 20;
 
 rgbImage = imresize(image, [512 512]);
 [rows, columns, numberOfColorBands] = size(rgbImage);
@@ -10,17 +10,55 @@ blockSize = 32;
 ca = mat2cell(rgbImage,blockSize*ones(1,size(rgbImage,1)/blockSize),blockSize*ones(1,size(rgbImage,2)/blockSize),3);
 
 LABvalues = ones(16,16,3);
-
+%find lab values
 for n = 1:16
     for j = 1:16
         temp = ca{n,j};
         temp = temp/255;
-        figure
-        imshow(temp)
+   
         temp = rgb2lab(temp);
         LABvalues(n,j,1) = mean(mean(temp(:,:,1)));
         LABvalues(n,j,2) = mean(mean(temp(:,:,2)));
         LABvalues(n,j,3) = mean(mean(temp(:,:,3)));     
     end
 end
+
+%delta e
+
+swapIndex = zeros(16,16);
+for n = 1:16
+   for j = 1:16
+       for k = 1:50
+            temp = LABvalue{k};
+            deltaEl = ( LABvalues(n,j,1)-temp(1) )^2; 
+            deltaEa = ( LABvalues(n,j,2)-temp(2) )^2; 
+            deltaEb = ( LABvalues(n,j,3)-temp(3) )^2; 
+            deltaE(k) = sqrt(deltaEl+deltaEa+deltaEb);
+                      
+       end
+       [~, swapIndex(n,j)] = min(deltaE);   
+   end
+end
+
+image2 = zeros(size(image));
+
+
+for n = 1:16
+   for j = 1:16
+       temp = imread(sprintf('DB/%d.jpg',swapIndex(n,j)));
+       rgbImage = imresize(temp, [blockSize blockSize]);
+       rgbImage = im2double(rgbImage);
+       image2( (1+( (n-1)*blockSize)):(n*blockSize) , (1+( (j-1)*blockSize)):(j*blockSize),:) = rgbImage;
+      
+   end
+end
+
+
+
+
+
+
+
+
+
 
